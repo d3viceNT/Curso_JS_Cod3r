615e371428f4b3548a3e01b2ac886d9f8096a97b
Marco <sgtmarcoaurelio@icloud.com>
2021-03-29 22:53:26 -0300

    :construction:

--- Fundamentos/operadores.js
+++ Fundamentos/operadores.js
@@ -47,6 +47,9 @@ const [, [, x, y]] = [[2, 3], ['a', 7, 9]] // Desestruturando elementos em array
 console.log(x)
 console.log(y)
 
+console.log()
+console.log('------------------')
+
 /* OPERADORES RELACIONAIS */
 
 console.log('01)', '1' == 1);
@@ -61,3 +64,24 @@ console.log('08)', 3 >= 2);
 
 console.log('09)', undefined == null);
 console.log('10)', undefined === null);
+
+console.log()
+console.log('------------------')
+
+
+/* OPERADORES LÓGICOS */
+
+function compras(trabalho1, trabalho2) {
+    const comprarSorvete = trabalho1 || trabalho2;
+    const comprarTV50 = trabalho1 && trabalho2;
+    // const comprarTV32 = !!(trabalho1 ^ trabalho2); // bitwise XOR
+    const comprarTV32 = trabalho1 != trabalho2; // XOR
+    const manterSaudavel = !comprarSorvete; // Operador unário
+
+    return { comprarSorvete, comprarTV50, comprarTV32, manterSaudavel }
+}
+
+console.log( compras( true, true ) )
+console.log( compras( true, false ) )
+console.log( compras( false, true ) )
+console.log( compras( false, false ) )
