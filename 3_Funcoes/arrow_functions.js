857fced10a2575699232ce1369f8b0451d256ff9
Marco <marcoaurelio_7@outlook.com>
2021-07-21 17:24:30 -0300

    :rocket::rocket::rocket:

--- 3_Funcoes/arrow_functions.js
+++ 3_Funcoes/arrow_functions.js
@@ -23,4 +23,23 @@ function Aluno() {
     }, 1000);
 }
 
-new Aluno
+// new Aluno
+
+console.log("\n--------------------\n");
+
+let comparaComThis = function(param) {
+    console.log(this === param);
+}
+
+comparaComThis(global); // Em funções comuns, this apontará para o objeto global (ou window no browser)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj) // Mudando o escopo do this arbitrariamente
+comparaComThis(global)
+
+let comparaComThisArrow = param => console.log(this === param) // Em funções Arrow, o this não apontará para o objeto global
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports) // module.exports === this em arrow functions (no node)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)  // Com Arrow functions, mesmo forçando, o this não mudará sua referência 
+comparaComThisArrow(obj)
--- 3_Funcoes/thisAndBind.js
+++ 3_Funcoes/thisAndBind.js
@@ -10,7 +10,7 @@ pessoa.falar(); // Uso natura do "this" dentro do escopo do objeto
 const fale = pessoa.falar; // Ao atribuir a função falar à constante "fale", estermos mudando o escopo em que o "this" será definido
 fale(); // Conflito no uso do "this"
 
-const faleComBind = pessoa.falar.bind(pessoa); // Resolvendo o escopo do "this" indicando implicitamente qual deve ser seu escopo 
+const faleComBind = pessoa.falar.bind(pessoa); // Resolvendo o escopo do "this" indicando arbitrariamente qual deve ser seu escopo 
 faleComBind();
 
 console.log("\n-------------------------------\n")
